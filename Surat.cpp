559dc2668f94bbaab6fe84ffc4b7ea3f56018815
Crzmaitri <113175916+Crzmaitri@users.noreply.github.com>
2022-09-30 16:49:22 +0700

    Create Surat.cpp

---
+++ Surat.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	cout<<"Halo \n"; 
+	cout<<"Jadi,ini tugas \n";
+	cout<<"Suruh buat surat\n";
+	cout<<"Tapi aku dak tau nak tulis atau buat ap\n";
+	
+	cout<<"Jadi...Halo apa kabar\n";
+	cout<<"Baik kan? \n";
+	cout<<"Okee sudah\n";
+	
+	cout<<"Terima kasih sudah baca\n";
+	
+	
+	
+	
+	return 0;
+}
